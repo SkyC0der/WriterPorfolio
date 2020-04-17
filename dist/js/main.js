64c7ad2ea306e2e7a6baa1f97f0d99761ee590df
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-04-17 23:51:57 +0100

    Created Writer Portfolio

---
+++ dist/js/main.js
@@ -0,0 +1,30 @@
+// Select DOM items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuBranding = document.querySelector('.menu-branding');
+const menuNav = document.querySelector('.menu-nav');
+const navItem = document.querySelectorAll('.nav-item');
+
+
+// Set Initial Stae Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu){
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuBranding.classList.add('show');
+        menuNav.classList.add('show');
+        navItem.forEach(item => item.classList.add('show'));
+        showMenu = true;
+    }else{
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuBranding.classList.remove('show');
+        menuNav.classList.remove('show');
+        navItem.forEach(item => item.classList.remove('show'));
+        showMenu = false;
+    }
+}
